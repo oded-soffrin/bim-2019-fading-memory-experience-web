cb421eb3ad243b3d0e323b84b9f0f01fa9fb15b4
Oded Soffrin <odeds@wix.com>
2019-09-10 09:05:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button'
 
 function App() {
+
   return (
     <div className="App">
+      <div className="App-logo" alt="logo">
+          BIM
+        </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome!</p>
+        
+        <Button
+          label={'Start!'}
+        />
+        
       </header>
+      
+      
     </div>
   );
 }
