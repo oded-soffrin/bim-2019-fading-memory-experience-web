e1dda5095b4135fb8c1e145e78e95f4b2ff805dd
Oded Soffrin <odeds@wix.com>
2019-10-26 13:38:13 +0300

    background image in home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,14 +3,16 @@ import Button from './Button2'
 import changeRouteFactory from '../utils/changeRouteFactory'
 import { useTranslation } from 'react-i18next';
 import Page from './Page'
+import {getImageSrc} from '../utils/global'
 
 import { withRouter } from 'react-router-dom';
 
 const HomePage = ({history}) => {
     const changeRoute = changeRouteFactory(history);
     const { t } = useTranslation();
+    const bgimage = getImageSrc(0)
     return (
-    <Page>
+    <Page className2='homePage' styles={{'background-image': `url(${bgimage})`}}>        
         <p>{t('HOME_WELCOME')}</p>
         
         <Button
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,14 +1,13 @@
 import React, {useEffect} from 'react';
 
-const Page = ({children, className2 = ''}) => {
+const Page = ({children, className2 = '', styles}) => {
 
-  
 
   useEffect(() => {
     window.keepalive();
   }, [])
   return (
-      <header className={`App-header ${className2}`}>
+      <header className={`App-header ${className2}`} style={styles}>
           
           {children}
       </header>
